9e692bcdc13e53edb9275bbee6ec48ed91ff7675
Jareth329 <130198302+Jareth329@users.noreply.github.com>
2023-12-28 23:29:03 -0600

    add ability to toggle titlebar
    - currently T

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,9 +1,16 @@
 extends Control
 
+var borderless:bool = false
+
 func _unhandled_input(event:InputEvent) -> void:
 	if event is InputEventKey:
 		if not event.pressed: return
 		if event.keycode == KEY_H:
 			Signals.update_visibility_ui.emit()
+		elif event.keycode == KEY_T:
+			_update_titlebar_visibility()
 		elif event.keycode == KEY_F8 or event.keycode == KEY_ESCAPE:
 			get_tree().quit()
+func _update_titlebar_visibility() -> void:
+	get_tree().root.borderless = not borderless
+	borderless = not borderless
--- scripts/signals.gd
+++ scripts/signals.gd
@@ -1,3 +1,4 @@
 extends Node
 
 signal update_counter(value, max_value)
+signal update_visibility_titlebar
