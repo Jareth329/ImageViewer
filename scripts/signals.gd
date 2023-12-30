8a0b5c1e08da1745f552109ac382fbc35ecaa681
Jareth329 <130198302+Jareth329@users.noreply.github.com>
2023-12-29 21:05:38 -0600

    fix natural sort
    - it was previously sorting only on the numbers in the file name, which works fine when images are named like 1.jpg, 2.jpg, but not any other time
    - main issue is that gdscript has no equivalent to csharp Regex.Split() that I am aware of; maybe it would be possible to make a regex that matches a sequence of numbers OR a sequence of any non-numbers in order to accomplish the same thing though

--- scripts/signals.gd
+++ scripts/signals.gd
@@ -11,15 +11,29 @@ class SortNatural:
 			return a < b
 		return a.to_int() < b.to_int()
 	
+	static func split(str:String, matches:Array[RegExMatch]) -> Array[String]:
+		var arr:Array[String] = []
+		var start:int = 0
+		for mat:RegExMatch in matches:
+			if start < mat.get_start(): # string first
+				arr.append(str.substr(start, mat.get_start() - start))
+			arr.append(mat.get_string())
+			start = mat.get_end()
+		return arr
+	
 	static func sort_natural(a:String, b:String) -> bool:
 		if a == b: return false
 		var a1:Array[RegExMatch] = regex_num.search_all(a)
 		var b1:Array[RegExMatch] = regex_num.search_all(b)
+		if a1.is_empty() or b1.is_empty(): return a < b
 		
-		for i:int in min(len(a1), len(b1)):
-			if a1[i].get_string() != b1[i].get_string():
-				return part_compare(a1[i].get_string(), b1[i].get_string())
+		var a2:Array[String] = split(a, a1)
+		var b2:Array[String] = split(b, b1)
 		
+		for i:int in min(a2.size(), b2.size()):
+			if a2[i] != b2[i]:
+				return part_compare(a2[i], b2[i])
+
 		if len(b1) > len(a1): 
 			return true
 		return false
